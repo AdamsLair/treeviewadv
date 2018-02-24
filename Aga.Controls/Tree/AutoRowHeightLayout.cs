8771f647676182270741a5645915352e6fc17028
Gal Cohen <galc@users.noreply.github.com>
2018-02-24 12:37:57 +0200

    Update AutoRowHeightLayout.cs (#7)
    #CHANGE: Avoid wasted enumeration of all row heights if the selection is on the header column.

--- Aga.Controls/Tree/AutoRowHeightLayout.cs
+++ Aga.Controls/Tree/AutoRowHeightLayout.cs
@@ -123,6 +123,8 @@ namespace Aga.Controls.Tree
 		public int GetRowAt(Point point)
 		{
 			int py = point.Y - _treeView.ColumnHeaderHeight;
+			if (py < 0)  // Header column
+				return -1; 
 			int y = 0;
 			for (int i = _treeView.FirstVisibleRow; i < _treeView.RowCount; i++)
 			{
