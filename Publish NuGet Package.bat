b6f6f410e2324f36a09d56526f95cf8944982590
Adam <adam@adamslair.net>
2019-05-01 12:04:28 +0200

    Updated Package to use Framework Folders

--- Publish NuGet Package.bat
+++ Publish NuGet Package.bat
@@ -1,6 +1,5 @@
-:: search for msbuild and make it available
-set bb.build.msbuild.exe=
-for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuildPath=%%D
+:: search for msbuild make it available
+set msbuildPath=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\
 set PATH=%PATH%;%msbuildPath%
 
 :: make sure we have a clean release build
@@ -13,7 +12,7 @@ del *.nupkg
 .nuget\nuget pack Aga.Controls\Aga.Controls.csproj -Properties Configuration=Release
 
 :: upload the nuget packages
-.nuget\nuget push *.nupkg
+.nuget\nuget push *.nupkg -Source "https://www.nuget.org"
 
 :: remove nupkg files after uploading them
 del *.nupkg
